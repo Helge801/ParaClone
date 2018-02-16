6101359d3e3692211d408d62b54351cc3d64325c
Bryan Lund <bryan.helge@gmail.com>
2018-02-16 06:44:17 -0700

    added static content to values controller

--- ValuesController.cs
+++ ValuesController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -15,8 +15,11 @@ namespace ParaClone
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "TestCode", "Hello" };
+            return new string[] { "REAL SOLUTIONS", "FOR GYMS MEMBERS","THE LATEST", "PARAMOUNT CUSTOMER SERVICE",
+                                  "Paramount Acceptance is here to help you","ABOUT","PARAMOUNT","Paramount Acceptance provides software and billing services in the fitness industry. With over 40 years of experience, Paramount is dedicated to serving our client’s customers with anything they need. From changing personal billing information to resolving a disputed charge, we are committed to helping you resolve any issues that might arise.",
+                                  "CONTACT","PARAMOUNT","Please fill out the form below with your inquiry.","A customer service representative will respond as soon as possible","PAY ONLINE NOW",
+                                  "Need to make a payment? Well now you can online.","Click on the button below to quickly make a payment today.",
+                                  "CONTACT INFORMATION","Contact us","PO BOX 12345 Somewhere","800.555.5555"};
         }
     }
 }
- 
